0e71305a1fac811500150d109eceec8a9433b8a7
Pranav <thepranavgupta@gmail.com>
2020-05-27 09:44:53 +0530

    a3 final

--- openMPI/a3.c
+++ openMPI/a3.c
@@ -21,24 +21,35 @@ int main(int argc,char *argv[])
             printf(" %d", arr[i]);
         printf("\n");
     }
+    int current_sum=0;
+
     if(world_size != NUMBER_OF_THREADS){
         printf("Please set number of threads as %d", NUMBER_OF_THREADS);
         return -1;
     }
+    
     if(my_rank == MASTER_PROCESS){
-        MPI_Send(&arr[my_rank], 1, MPI_INT, my_rank+1, 0, MPI_COMM_WORLD);
+        current_sum = arr[0];
+        for (int i = 1; i < NUMBER_OF_THREADS; i++){
+            MPI_Send(&current_sum, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Recv(&current_sum, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+            arr[i] = current_sum;
+        }
+        // MPI_Send(&arr[my_rank], 1, MPI_INT, my_rank+1, 0, MPI_COMM_WORLD);
     }
-    else if(my_rank == NUMBER_OF_THREADS-1){
-        MPI_Recv(&val, 1, MPI_INT, my_rank-1, 0, MPI_COMM_WORLD, &status);
-        arr[my_rank] += val;
-    }
+    // else if(my_rank == NUMBER_OF_THREADS-1){
+    //     MPI_Recv(&val, 1, MPI_INT, my_rank-1, 0, MPI_COMM_WORLD, &status);
+    //     arr[my_rank] += val;
+    // }
     else{
-        MPI_Recv(&val, 1, MPI_INT, my_rank-1, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(&val, 1, MPI_INT, MASTER_PROCESS, 0, MPI_COMM_WORLD, &status);
         arr[my_rank] += val;
-        MPI_Send(&arr[my_rank], 1, MPI_INT, my_rank+1, 0, MPI_COMM_WORLD);
+        MPI_Send(&arr[my_rank], 1, MPI_INT, MASTER_PROCESS, 1, MPI_COMM_WORLD);
     }
-    if(my_rank == NUMBER_OF_THREADS-1){
-        printf("Prefix calculations: ");
+
+
+    if(my_rank == MASTER_PROCESS){
+        printf("Prefix calculation: ");
         for(int i = 0; i < NUMBER_OF_THREADS; i++)
             printf(" %d", arr[i]);
         printf("\n");
